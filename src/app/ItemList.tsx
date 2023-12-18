b0fe4decf5d0bd4f40c80540dc18d0b8a1ee1dcd
TBSten <programmingcafeteria@gmail.com>
2023-12-18 15:57:29 +0900

    add dynamic function call

--- src/app/ItemList.tsx
+++ src/app/ItemList.tsx
@@ -1,7 +1,9 @@
 import { sleep } from "../util/sleep"
+import {headers} from "next/headers"
 
 const getItems = async ()=>{
   await sleep(3000)
+  console.log(Array.from(headers().entries()))
   return [
     { id: "test-1", name: "chocolate" },
     { id: "test-2", name: "rice ball" },
